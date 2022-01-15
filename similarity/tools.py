27fbe78741f3c144432b0ce389841800219cc412
KenyonYan <1806094668@qq.com>
2022-01-15 23:59:20 +0800

    路径工具类，统一管理路径

---
+++ similarity/tools.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+import pathlib
+import os
+
+root_path = str(pathlib.Path(os.path.abspath(__file__)).parent.parent)  # 路径为 E:\PythonProject\Similarity
+data_dir = os.path.join(root_path, 'similarity\\data\\')
+result_dir = os.path.join(root_path, 'similarity\\result\\')
+model_dir = os.path.join(root_path, 'similarity\\model\\')
+
+# print(root_path)
